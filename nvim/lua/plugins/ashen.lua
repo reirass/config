86b5a0c41e913bf9080f7ffa84a84e9a13a0ae86
Matheus Pereira <78914056+reirass@users.noreply.github.com>
2025-10-28 18:17:49 -0300

    Add black-metal-theme configuration to ashen.lua

--- nvim/lua/plugins/ashen.lua
+++ nvim/lua/plugins/ashen.lua
@@ -1,9 +1,12 @@
 return {
-  { "datsfilipe/vesper.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "vesper",
-    },
-  },
+  "metalelf0/black-metal-theme-neovim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("black-metal").setup({
+      theme = "thyrfing",
+      variant = "dark",
+    })
+    require("black-metal").load()
+  end,
 }
