cb8da3539cb7226b3ae35be878002915f81ca54e
Matheus Pereira <78914056+reirass@users.noreply.github.com>
2025-10-28 18:17:09 -0300

    Add new keymaps for visual and normal modes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,4 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set('n', '<C-p>', vim.cmd.ColorizerToggle)
+vim.keymap.set("n", "<C-p>", vim.cmd.ColorizerToggle)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "Q", "<nop>")
